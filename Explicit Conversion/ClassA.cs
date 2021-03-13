e4153d1e4a5646eb99fbe21585c1f8cda8c8ad91
Revotap <44678873+Revotap@users.noreply.github.com>
2021-03-13 13:38:22 +0100

    Added Explicit Conversion sample

---
+++ Explicit Conversion/ClassA.cs
@@ -0,0 +1,26 @@
+﻿/*
+    Author: Leon Grell
+ */
+
+using System;
+
+namespace Revoap_Explicit_Conversion
+{
+   class ClassA
+    {
+        public int MyValue = 0;
+        public string MyText = "Hello World!";
+
+        /*Explicit Conversion to int*/
+        public static explicit operator int(ClassA x)
+        {
+            return x.MyValue;
+        }
+
+        /*Explicit Conversion to string*/
+        public static explicit operator string(ClassA x)
+        {
+            return x.MyText;
+        }
+    }
+}
---
+++ Explicit Conversion/Demo.cs
@@ -0,0 +1,26 @@
+﻿/*
+    Auhtor: Leon Grell
+ */
+
+using System;
+
+namespace Revoap_Explicit_Conversion
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            ClassA myObjekt = new ClassA();
+            myObjekt.MyValue = 13;
+            myObjekt.MyText = "Hello guys!";
+
+            /*Explicit conversion to int*/
+            int x = (int)myObjekt;
+
+            /*Explicit conversion to string*/
+            string str = (string)myObjekt;
+
+            Console.WriteLine("X: {0}, STR: {1}", x, str);
+        }
+    }
+}
