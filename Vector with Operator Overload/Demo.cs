1171fcefce17320652f0fd1cb5b5c4be53c41728
Revotap <44678873+Revotap@users.noreply.github.com>
2021-03-13 14:00:47 +0100

    Added Vector with operator overload

---
+++ Vector with Operator Overload/Demo.cs
@@ -0,0 +1,27 @@
+﻿/*
+    Author: Leon Grell    
+*/
+
+using System;
+
+namespace Vector_with_Operator_Overload
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vector Test");
+
+            Vector v1 = new Vector(1, 2, 3);
+            Vector v2 = new Vector(2, 3, 4);
+            Vector v3 = new Vector(3, 3, 3);
+
+            Console.Out.WriteLine("v1: {0}", v1);
+            Console.Out.WriteLine("v2: {0}", v2);
+            Console.Out.WriteLine("v3: {0}", v3);
+            Console.Out.WriteLine("v1 + v2: {0}", v1 + v2);
+            Console.Out.WriteLine("v2 + 2: {0}", v2 + 2);
+            Console.Out.WriteLine("-v3: {0}", -v3);
+        }
+    }
+}
---
+++ Vector with Operator Overload/Vector.cs
@@ -0,0 +1,125 @@
+﻿/*
+    Author: Leon Grell    
+*/
+
+namespace Vector_with_Operator_Overload
+{
+    class Vector
+    {
+        private float x;
+        private float y;
+        private float z;
+
+        /*Constructors*/
+        public Vector()
+        {
+            x = 0;
+            y = 0;
+            z = 0;
+        }
+
+        public Vector(float n_x, float n_y, float n_z)
+        {
+            x = n_x;
+            y = n_y;
+            z = n_z;
+        }
+
+        /*Getter and Setter*/
+        float getX()
+        {
+            return x;
+        }
+        void setX(float n_x)
+        {
+            x = n_x;
+        }
+        float getY()
+        {
+            return y;
+        }
+        void setY(float n_y)
+        {
+            y = n_y;
+        }
+        float getZ()
+        {
+            return z;
+        }
+        void setZ(float n_z)
+        {
+            z = n_z;
+        }
+
+        /*Operator Overloads*/
+
+        /*Unary Operator -*/
+        public static Vector operator -(Vector v1)
+        {
+            Vector tmp = new Vector();
+            tmp.setX(v1.x - 1);
+            tmp.setY(v1.y - 1);
+            tmp.setZ(v1.z - 1);
+
+            return tmp;
+        }
+        /*Unary Operator +*/
+        public static Vector operator +(Vector v1)
+        {
+            Vector tmp = new Vector();
+            tmp.setX(v1.x + 1);
+            tmp.setY(v1.y + 1);
+            tmp.setZ(v1.z + 1);
+
+            return tmp;
+        }
+
+        /*Binary Operator +, two vectors*/
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            Vector tmp = new Vector();
+            tmp.setX(v1.getX() + v2.getX());
+            tmp.setY(v1.getY() + v2.getY());
+            tmp.setZ(v1.getZ() + v2.getZ());
+
+            return tmp;
+        }
+        /*Binary Operator -, two vectors*/
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            Vector tmp = new Vector();
+            tmp.setX(v1.getX() - v2.getX());
+            tmp.setY(v1.getY() - v2.getY());
+            tmp.setZ(v1.getZ() - v2.getZ());
+
+            return tmp;
+        }
+
+        /*Binary Operator +, vector + konstant*/
+        public static Vector operator +(Vector v1, int k)
+        {
+            Vector tmp = new Vector();
+            tmp.setX(v1.getX() + k);
+            tmp.setY(v1.getY() + k);
+            tmp.setZ(v1.getZ() + k);
+
+            return tmp;
+        }
+        /*Binary Operator -, vector - konstant*/
+        public static Vector operator -(Vector v1, int k)
+        {
+            Vector tmp = new Vector();
+            tmp.setX(v1.getX() - k);
+            tmp.setY(v1.getY() - k);
+            tmp.setZ(v1.getZ() - k);
+
+            return tmp;
+        }
+
+        /*Override toString for output*/
+        public override string ToString()
+        {
+            return "(" + x + ", " + y + ", " + z + ")";
+        }
+    }
+}
